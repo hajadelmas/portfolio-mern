22c761f66e4ca3d9f8cf8995e7303aea862aa3e5
Loic Delmas <loichdelmas@gmail.com>
2020-07-21 16:40:36 +0200

    mobile

--- studio_haja/src/App.js
+++ studio_haja/src/App.js
@@ -7,6 +7,7 @@ import Portfolio from './components/Portfolio'
 import Offres from './components/Offres'
 import Contact from './components/Contact'
 import Apropos from './components/Apropos'
+import AppMobile from './components/mobile/AppMobile'
 
 class App extends Component {
   constructor () {
@@ -26,30 +27,38 @@ class App extends Component {
     const { showComponentCount } = this.state
 
     return (
-      <div className='section'>
-        <img src={logoSVG} class='logo1' alt='' onClick={() => this.showComponent(0)} />
+      <div>
+        <div className='section'>
+          <img src={logoSVG} class='logo1' alt='' onClick={() => this.showComponent(0)} />
 
-        <div className='connect sweep-to-right'>
-          <Link to='/login'>Espace client</Link>
-          <i className='fas fa-user-circle userLogo' />
+          <div className='connect sweep-to-right'>
+            <Link to='/login'>Espace client</Link>
+            <i className='fas fa-user-circle userLogo' />
+          </div>
+
+          <div className='menu1'>
+            <ul>
+              <li className='hvr-float underline-from-left' onClick={() => this.showComponent(1)}>Portfolio</li>
+              <li className='hvr-float underline-from-left' onClick={() => this.showComponent(2)}>Offres</li>
+              <li className='hvr-float underline-from-left' onClick={() => this.showComponent(3)}>A propos</li>
+              <li className='hvr-float underline-from-left' onClick={() => this.showComponent(4)}>Contact</li>
+            </ul>
+          </div>
+
+          {showComponentCount === 0 ? <Main /> : null}
+          {showComponentCount === 1 ? <Portfolio /> : null}
+          {showComponentCount === 2 ? <Offres /> : null}
+          {showComponentCount === 3 ? <Apropos /> : null}
+          {showComponentCount === 4 ? <Contact /> : null}
+
         </div>
 
-        <div className='menu1'>
-          <ul>
-            <li className='hvr-float underline-from-left' onClick={() => this.showComponent(1)}>Portfolio</li>
-            <li className='hvr-float underline-from-left' onClick={() => this.showComponent(2)}>Offres</li>
-            <li className='hvr-float underline-from-left' onClick={() => this.showComponent(3)}>A propos</li>
-            <li className='hvr-float underline-from-left' onClick={() => this.showComponent(4)}>Contact</li>
-          </ul>
+        <div className='mobile'>
+          <AppMobile />
         </div>
-
-        {showComponentCount === 0 ? <Main /> : null}
-        {showComponentCount === 1 ? <Portfolio /> : null}
-        {showComponentCount === 2 ? <Offres /> : null}
-        {showComponentCount === 3 ? <Apropos /> : null}
-        {showComponentCount === 4 ? <Contact /> : null}
 
       </div>
+
     )
   }
 }
